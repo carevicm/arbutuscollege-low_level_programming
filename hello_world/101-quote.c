79c696f4c9b93c08ce247df17400512721317663
carevicm <milancarevic@yahoo.com>
2023-02-14 19:19:25 -0800

    Eight commit

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
- * main -Entry point
- * Return: Always 1 (Success)
+ * main - Entry point
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is usefu - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
