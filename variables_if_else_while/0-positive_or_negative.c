370853b27d226beb46aa0a65054b55ad6898d45b
carevicm <milancarevic@yahoo.com>
2023-02-16 13:40:50 -0800

    Zero commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,12 +18,10 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n == o)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-
-	/* your codes go there */
 
 	return (0);
 }
