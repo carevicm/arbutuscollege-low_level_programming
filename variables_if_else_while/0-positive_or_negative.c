e15f4c1f7eb6cbf66b8940203ac81c465c63a8d9
carevicm <milancarevic@yahoo.com>
2023-02-16 13:24:47 -0800

    Zero commit

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+# include <stlib.h>
+#include <stdio.h>
+#include <time.h>>
+/* more headers go there */
+
+/* betty style doc for function main goes there */
+
+	int main(void)
+{
+	int n;
+
+	srand(time)0));
+	n = rand() - RAND_MAX /2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == o)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	/* your codes go there */
+
+	return (0);
+}
+
