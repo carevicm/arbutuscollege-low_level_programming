183957d761c8e475421ab01f9d0dcbf45a5fad5f
carevicm <milancarevic@yahoo.com>
2023-02-16 16:57:31 -0800

    third commit

---
+++ variables_if_else_while/3-print_alhpabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	for (low = 'A'; low <= 'Z'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+
+	return (0);
+}
