79ce94f684ec0c6eee2a41f64dae9b1b500d4d17
carevicm <milancarevic@yahoo.com>
2023-02-16 19:13:57 -0800

    twelveth commit

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -21,17 +21,16 @@ int main(void)
 			putchar((p % 10) + '0');
 			putchar(' ');
 			putchar((q / 10) + '0');
-			putchar(( q % 10) + '0');
-			
+			putchar((q % 10) + '0');
+
 			if (p == 98 && q == 99)
 				continue;
 
-			
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
