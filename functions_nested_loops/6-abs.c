15500b8c5b2db1a0b8eeac9f0c706e2e0e8c6b54
carevicm <milancarevic@yahoo.com>
2023-02-17 15:50:13 -0800

    sixth commit

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *_abs - Check Holberton
- * @r: An integre input
+ *_abs - Check main
+ * @r: integre input
  * Description: This function returns absolute value of a number
  * Return: Absolut value of number r
  */
