d4776b85851a5110fd6365e7d87a0018652b22f0
carevicm <milancarevic@yahoo.com>
2023-02-16 18:59:38 -0800

    twelth commit

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d, p;
+
+	for (p = 0; p <= 98; p++)
+	{
+		for (q = p + 1; q <= 99; q++)
+		{
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			puthcar(' ');
+			putchar((q / 10) + '0');
+			putchar(( q % 10) + '0');
+
+			if (p == 98 && q == 99);
+			continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
