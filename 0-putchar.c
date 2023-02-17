fa03bde75f3e0b148057ba10968a8355b8e406d8
carevicm <milancarevic@yahoo.com>
2023-02-16 23:51:25 -0800

    zero commit

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	char word[9] = _putchar;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(word[i]);
+	
+	_putchar('\n');
+
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-C-programming
+Functions, nested, loops
