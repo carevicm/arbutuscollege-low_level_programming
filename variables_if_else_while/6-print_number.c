a330a7d289d80cffc8a226126d2fdcb8d1afd1d4
carevicm <milancarevic@yahoo.com>
2023-02-16 17:24:23 -0800

    sixth commit

---
+++ variables_if_else_while/6-print_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <'9'; d++)
+		putchar(d);
+	putchar("\n");
+
+	return (0);
+}
