b04c1e234370918b2512fdf04d05175901ead8c7
carevicm <milancarevic@yahoo.com>
2023-02-16 17:10:25 -0800

    fourth commmit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+	
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	putchar('\n');
+
+	return (0);
+}
