64efbe1f43e34809362d1435b54b3e2675414c07
carevicm <milancarevic@yahoo.com>
2023-02-16 16:24:49 -0800

    second commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	putchar('n\');
+	return (0);
+}
