6717c963043aa4d2fc0c548996cae02cb5c0bac0
carevicm <milancarevic@yahoo.com>
2023-02-17 16:06:11 -0800

    tenth commit

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * add - Check Holberton
+ * add - Check mainn
  * @a: An integer a
  * @b: An integer b
  * Description: Function that adds two integers and returns the result
