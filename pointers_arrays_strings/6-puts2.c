9fcddfccbfc1bf24413642bea575cefd0ddf484a
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-13 17:14:14 -0700

    sixth commit

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,12 @@
  */
 void puts2(char *str)
 {
-	int index = 0, len = 0;
+	int a;
 
-	while (str[index++])
-		len++;
-
-	for (index = 0; index < len; index += 2)
-		_putchar(str[index]);
-
+	for (a = 0; *(str + a) != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar(*(str + a));
+	}
 	_putchar('\n');
 }
