82048bd09f2e5c228ac5f4102600f875214d6957
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-07-11 20:11:46 -0700

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,12 +1,19 @@
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+
+int		_putchar(char);
+void	reset_to_98(int *);
+void	swap_int(int *, int *);
+int		_strlen(char *);
+void	_puts(char *);
+void	print_rev(char *);
+void	rev_string(char *);
+void	puts2(char *);
+void	puts_half(char *);
+void	print_array(int *, int);
+char	*_strcpy(char *, char *);
+int		_atoi(char *);
+
+#endif
